8ed248202ed41d35b985f4b17ef22e40b9d800d4
yrc <958480638@qq.com>
2023-11-21 23:46:48 +0800

    feat: 实现Policy，Stats配置类

--- src/main/kotlin/com/kebab/v2rayk/wrapper/config/V2rayProperties.kt
+++ src/main/kotlin/com/kebab/v2rayk/wrapper/config/V2rayProperties.kt
@@ -4,9 +4,9 @@ data class V2rayProperties(
 	val log: Log? = null,
 	val api: Api? = null,
 	val dns: Dns? = null,
-//	val stats: Stats? = null,
+	val stats: Stats? = null,
 	val routing: Routing? = null,
-//	val policy: Policy? = null
+	val policy: Policy? = null,
 //	val reverse: Reverse? = null,
 	val inbounds: List<Inbound>? = null,
 //	val outbounds: List<Outbounds>? = null,
